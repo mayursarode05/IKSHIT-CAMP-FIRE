ee02c18b3ebaffa963da169ac1f0680e5d2ca199
MayurSarode <mayursarode333@gmail.com>
2024-10-16 12:10:09 +0530

    Add image

--- src/app/shared/components/index/index.component.ts
+++ src/app/shared/components/index/index.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
 
+  constructor(private route:Router){}
+
+  NavigateUser = () => {
+    this.route.navigate(['/contactUs'])
+  }
 }
